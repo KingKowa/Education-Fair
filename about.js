9c8f4c3b95bac3372e6955b341bde43f91fc0688
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-27 09:40:22 +0000

    Fix indentation errors with about.js file

--- about.js
+++ about.js
@@ -2,11 +2,11 @@ const hamburgerIcon = document.querySelector('.ham-icon');
 const mobileMenu = document.querySelector('.mobile-nav-menu');
 const mobileMenuItems = document.querySelectorAll('.mobile-nav-menu a');
 hamburgerIcon.addEventListener('click', () => {
-    hamburgerIcon.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-  });
+  hamburgerIcon.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
 
 mobileMenuItems.forEach((event) => event.addEventListener('click', () => {
-    hamburgerIcon.classList.remove('active');
-    mobileMenu.classList.remove('active');
-}))
+  hamburgerIcon.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
