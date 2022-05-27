3bb1c8a10e0cfdfaf74a2bab04410e8f6e51d2e9
KingKowa <ankomahfrederickyahoo@gmail.com>
2022-05-27 09:33:40 +0000

    Fix indentation errors with main.js file

--- main.js
+++ main.js
@@ -3,42 +3,42 @@ const speakers = [
     name: 'Sam Jonah',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
   {
     name: 'Isaac Acquah',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
   {
     name: 'P.Y Okyere',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
   {
     name: 'Anastasia Yiadom',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
   {
     name: 'Emmanuel Kyei Baffour',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
   {
     name: 'Swanzy Obeng',
     image: './images/guest-speaker.png',
     title: 'Professor- Dean Of the Faculty of Applied Science',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.' ,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident explicabo voluptates magnam maxime adipisci! Alias.',
   },
 ];
 
 speakers.forEach((speaker) => {
-    const speakerDetails = `
+  const speakerDetails = `
       <div class="speakers">
           <img class="speaker-img" src="${speaker.image}" alt="featured speaker one">
           <div class="descriptions">
@@ -48,18 +48,18 @@ speakers.forEach((speaker) => {
               </p>
           </div>
       </div>`;
-    document.getElementById('guest-speakers').innerHTML += speakerDetails;
-  });
+  document.getElementById('guest-speakers').innerHTML += speakerDetails;
+});
 
 const hamburgerIcon = document.querySelector('.ham-icon');
 const mobileMenu = document.querySelector('.mobile-nav-menu');
 const mobileMenuItems = document.querySelectorAll('.mobile-nav-menu a');
 hamburgerIcon.addEventListener('click', () => {
-    hamburgerIcon.classList.toggle('active');
-    mobileMenu.classList.toggle('active');
-  });
+  hamburgerIcon.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+});
 
 mobileMenuItems.forEach((event) => event.addEventListener('click', () => {
-    hamburgerIcon.classList.remove('active');
-    mobileMenu.classList.remove('active');
-}))
+  hamburgerIcon.classList.remove('active');
+  mobileMenu.classList.remove('active');
+}));
